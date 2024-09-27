c97c70a7d6ccd722fb14612135b69e8abc8eb36b
Hemanth katuri <135100157+HemanthKaturi19@users.noreply.github.com>
2024-09-27 10:25:51 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,10 +9,14 @@ const Navbar = () => {
   return (<nav className="  flex items-center justify-between py-6">
     <div className="text-5xl text-gray-200 font-semibold pl-6 pr-12">HK</div>
     <div className="flex justify-around items-center gap-4 text-3xl text-gray-200 pr-4">
-      <FaLinkedin />
-      <FaGithub />
-      <FaInstagram />
-      <FaSquareXTwitter />
+      <a href="https://www.linkedin.com/in/katuri-hemanth-018a1028a" target="_blank"><FaLinkedin />
+      </a>
+      <a href="https://github.com/HemanthKaturi19" target="_blank"><FaGithub />
+      </a>
+      <a href="https://www.instagram.com/hemanth_katuri_19/" target="_blank"><FaInstagram />
+      </a>
+      <a href="https://x.com/KaturiHemanth1" target="_blank"><FaSquareXTwitter />
+      </a>
     </div>
   </nav>)
 }

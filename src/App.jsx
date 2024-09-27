3bec51eccce8733d6ed9ace00b2b19bd79e89b69
Hemanth katuri <135100157+HemanthKaturi19@users.noreply.github.com>
2024-09-27 10:34:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Contact from './components/Contact.jsx'
 const App = () => {
   return (
    <div className="overflow-x-hidden  text-neutral-300 antialiased selection bg-cyan-300 selection:text-cyan-900 ">
-      <div className="absolute top-0 z-[-2] h-max w-auto mx-12 bg-neutral-950 bg-radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+      <div className="absolute top-0 z-[-2] h-max w-auto px-12 bg-neutral-950 bg-radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
         <div className="container px-4 ">
         <Navbar />
         <Hero />
